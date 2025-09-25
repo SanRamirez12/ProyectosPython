09bd6c070bc089951e8a400b8ddbd8bafe31659b
Santiago Ramírez Elizondo <sanramel1205@gmail.com>
2025-09-24 20:18:23 -0600

    Primeros pasos curso Universidad Python

---
+++ ClasesCursoUdemy/primeros_pasos.py
@@ -0,0 +1,6 @@
+#Programa inicial
+#print("Hola Mundo")
+nombre = 'Santiago Ramirez'
+edad = 22
+pais = 'Costa Rica'
+print(f'Hola, soy {nombre}, tengo {edad} años y soy de {pais}')
---
+++ ClasesCursoUdemy/variables.py
@@ -0,0 +1,6 @@
+#Variables en Python
+
+#Declaracion e inicializacion de variables
+edad = 22
+altura = 1.85
+pais = 'Costa Rica'
